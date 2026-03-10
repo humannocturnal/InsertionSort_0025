1fff3980bd1eda3bface776ea3f57f6084288dd4
humannocturnal <syaputrar374@gmail.com>
2026-03-10 10:42:14 +0700

    menambahkan prosedur insertion sort 2

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -34,5 +34,16 @@ void insertionSort() { //procedure insertion sort
     for (int i = 0; i < n; i++) {
         int key = arr[i];
         int j = i - 1;
+        while (j >= 0 && arr[j] > temp)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = temp;
+        cout << "\nStep" << i << ":";
+        for (int k = 0; k < n; k++) 
+        {
+            cout << arr[k] << " ";
+        }
     }
 }
