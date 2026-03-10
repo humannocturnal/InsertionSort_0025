bebf19e399ae153550077f216d4dc9a3b8ded081
humannocturnal <syaputrar374@gmail.com>
2026-03-10 10:52:06 +0700

    menampilkan elemen array

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -53,4 +53,9 @@ void display() {
     cout << "\n=================";
     cout << "\nElement Array" << endl;
     cout << "\n=================";
+
+    for(int j = 0; j < n; j++) {
+        cout << arr[j] << endl;
+    }
+    cout << endl;
 }
